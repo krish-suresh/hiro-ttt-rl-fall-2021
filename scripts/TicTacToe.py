a2308197bd6dc4c79368a00d4758eb9c555f0a23
Krishna Suresh <krishna4132@gmail.com>
2021-10-03 17:59:35 -0400

    progress on moving bot

--- scripts/TicTacToe.py
+++ scripts/TicTacToe.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 from robot import Robot
 from hiro_core.XamyabRobot import XamyabRobot, rospy
-
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 
 class TicTacToe:
     def __init__(self):
         self.robot = XamyabRobot(visualize_trajectory=True)
-    def play():
+    def play(self):
+        pose_goal = Pose()
+        pose_goal.orientation.w = 1.0
+        pose_goal.position.x = 0.4
+        pose_goal.position.y = 0.1
+        pose_goal.position.z = 0.4
+        self.robot.right_manipulator.set_pose_goal()
         # Draw Board
         # Randomize Starting player
         # loop while game not done:
@@ -21,4 +27,5 @@ class TicTacToe:
 
 if __name__ == "__main__":
     tictactoe = TicTacToe()
+    input("Move Robot")
     tictactoe.play()
