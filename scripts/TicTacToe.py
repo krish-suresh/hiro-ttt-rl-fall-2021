9543f56947e9d04c8c587bf20f652ba45d0fdb81
Krishna Suresh <krishna4132@gmail.com>
2021-10-03 21:37:32 -0400

    progress on bot

--- scripts/TicTacToe.py
+++ scripts/TicTacToe.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
-from robot import Robot
+from math import pi
+from tf.transformations import quaternion_from_euler
 from hiro_core.XamyabRobot import XamyabRobot, rospy
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+
+
+# roslaunch ur_gazebo xamyab.launch
+# roslaunch xamyab_moveit_config xamyab_moveit_planning_execution.launch
 
 class TicTacToe:
     def __init__(self):
         self.robot = XamyabRobot(visualize_trajectory=True)
+        self.default_gripper_quaternion = Quaternion(*quaternion_from_euler(pi, 0, pi / 2))
+        pose_goal = Pose(position=Point(*[0.6256, -0.50, 0.2]), orientation=self.default_gripper_quaternion)
+        self.robot.right_manipulator.set_pose_goal(pose_goal)
     def play(self):
-        pose_goal = Pose()
-        pose_goal.orientation.w = 1.0
-        pose_goal.position.x = 0.4
-        pose_goal.position.y = 0.1
-        pose_goal.position.z = 0.4
-        self.robot.right_manipulator.set_pose_goal()
+
         # Draw Board
         # Randomize Starting player
         # loop while game not done:
@@ -27,5 +30,3 @@ class TicTacToe:
 
 if __name__ == "__main__":
     tictactoe = TicTacToe()
-    input("Move Robot")
-    tictactoe.play()
